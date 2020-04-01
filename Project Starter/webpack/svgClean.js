09370d12a2c5e09829ee293c03c0a6474d7aa541
anton <youz.sani4@gmail.com>
2020-04-01 13:02:56 +0300

    front

--- Project Starter/webpack/svgClean.js
+++
@@ -1,5 +0,0 @@
-module.exports = function svgClean(source) {
-    source = source.replace(/(fill=".+?"|stroke=".+?")/g, '');
-    source = source.replace(/(>)\n(<)/g, '$1$2');
-    return source;
-};
